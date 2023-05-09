fd1ca400ce21a798798e0ddcff317781343d75b2
atish.gurumurthi <atish.gurumurthi@gmail.com>
2023-05-09 14:37:46 -0400

    RLO: First commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("First commit on all branches: dev, staging & prod.");
+
+        //Design calculator
+        //RLO-1111: Setup framework (classes, interfaces)
+        //RLO-2222: Add add functionality
+        //RLO-3333: Add subtract functionality
+        //RLO-4444: Unit testing
+        //Final checking - comments etc.
+
+        //        staging -> RLO-1111 -> push RLO-1111 -> merge develop -> merge staging
+        //        staging -> RLO-2222 -> push RLO-2222 -> merge develop -> merge staging
+        //        staging -> RLO-3333 -> push RLO-3333 -> merge develop -> merge staging
+        //        staging -> RLO-4444 -> push RLO-4444 -> merge develop -> merge staging
+        //        staging -> RLO-5555 -> push RLO-5555 -> merge develop -> merge staging
+
+
+    }
+}
